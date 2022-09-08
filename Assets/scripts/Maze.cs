76997bf6260536166ae188baccc70146928bf5c8
Warren Roche <warrengamedev@gmail.com>
2022-09-08 12:06:02 +0200

    updated code for placing walkable pieces
    added code for each type of maze piece and its rotation

--- Assets/scripts/Maze.cs
+++ Assets/scripts/Maze.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapLocation       
+public class MapLocation
 {
     public int x;
     public int z;
@@ -27,6 +27,31 @@ public class Maze : MonoBehaviour
     public int scale = 6;
 
     public GameObject straight;
+    public GameObject cornerCurved; // use for bottom or top right (-90 on y)
+    public GameObject cornerStraight; // use for bottom or top left (90 on y)
+    public GameObject crossroads;
+    public GameObject junction;
+    public GameObject deadend;
+
+    // patterns
+    private int[] verticalStraight = new int[] { 5, 0, 5, 1, 0, 1, 5, 0, 5 };
+    private int[] horizontalStraight = new int[] { 5, 1, 5, 0, 0, 0, 5, 1, 5 };
+    private int[] crossroadsPattern = new int[] { 5, 0, 5, 0, 0, 0, 5, 0, 5 };
+
+    private int[] junctionTop = new int[] { 5, 0, 5, 0, 0, 0, 5, 1, 5 }; // entering top
+    private int[] junctionBottom = new int[] { 5, 1, 5, 0, 0, 0, 5, 0, 5 }; // entering bottom
+    private int[] junctionLeft = new int[] { 5, 0, 5, 0, 0, 1, 5, 0, 5 }; // entering left
+    private int[] junctionRight = new int[] { 5, 0, 5, 1, 0, 0, 5, 0, 5 }; // entering right
+
+    private int[] deadendTop = new int[] { 5, 0, 5, 1, 0, 1, 5, 1, 5 };
+    private int[] deadendLeft = new int[] { 5, 1, 5, 0, 0, 1, 5, 1, 5 };
+    private int[] deadendBottom = new int[] { 5, 1, 5, 1, 0, 1, 5, 0, 5 };
+    private int[] deadendRight = new int[] { 5, 1, 5, 1, 0, 0, 5, 1, 5 };
+
+    private int[] cornerLeftTop = new int[] { 5, 0, 5, 0, 0, 1, 5, 1, 5 };
+    private int[] cornerLeftBottom = new int[] { 5, 1, 5, 0, 0, 1, 5, 0, 5 };
+    private int[] cornerRightBottom = new int[] { 5, 1, 5, 1, 0, 0, 5, 0, 5 };
+    private int[] cornerRightTop = new int[] { 5, 0, 5, 1, 0, 0, 5, 1, 5 };
 
     private readonly int wildcard = 5;
 
@@ -40,11 +65,11 @@ public class Maze : MonoBehaviour
 
     void InitialiseMap()
     {
-        map = new byte[width,depth];
+        map = new byte[width, depth];
         for (int z = 0; z < depth; z++)
             for (int x = 0; x < width; x++)
             {
-                    map[x, z] = 1;     //1 = wall  0 = corridor
+                map[x, z] = 1;     //1 = wall  0 = corridor
             }
     }
 
@@ -53,8 +78,8 @@ public class Maze : MonoBehaviour
         for (int z = 0; z < depth; z++)
             for (int x = 0; x < width; x++)
             {
-               if(Random.Range(0,100) < 50)
-                 map[x, z] = 0;     //1 = wall  0 = corridor
+                if (Random.Range(0, 100) < 50)
+                    map[x, z] = 0;     //1 = wall  0 = corridor
             }
     }
 
@@ -66,31 +91,112 @@ public class Maze : MonoBehaviour
                 Vector3 pos = new Vector3(x * scale, 0, z * scale);
                 if (map[x, z] == 1)
                 {
+                    // draws a white wall ie non-walkable block
                     GameObject wall = GameObject.CreatePrimitive(PrimitiveType.Cube);
                     wall.transform.localScale = new Vector3(scale, scale, scale);
                     wall.transform.position = pos;
-                } else if(ShouldPlaceVerticalStraight(x,z))
-                {
-                    Instantiate(straight, pos, Quaternion.identity);
                 }
-                else if (ShouldPlaceHorizontalPiece(x, z))
+                else
                 {
-                    Instantiate(straight, pos, Quaternion.Euler(0, 90, 0));
+                    PlaceWalkablePiece(z, x, pos);
                 }
+
             }
     }
 
-    bool ShouldPlaceVerticalStraight(int x, int z)
+    private void PlaceWalkablePiece(int z, int x, Vector3 pos)
     {
-        return Search2D(x, z, new int[] { 5, 0, 5, 1, 0, 1, 5, 0, 5 });
+        int[] matchedPattern = HasMatchedPattern(x, z, new int[][] { verticalStraight, horizontalStraight,
+                        cornerLeftBottom, cornerLeftTop, cornerRightBottom, cornerRightTop,
+                        crossroadsPattern,
+                        deadendBottom, deadendLeft, deadendRight, deadendTop,
+                        junctionBottom, junctionLeft, junctionRight, junctionTop });
+        if (matchedPattern == verticalStraight)
+        {
+            Instantiate(straight, pos, Quaternion.identity);
+        }
+        else if (matchedPattern == horizontalStraight)
+        {
+            Instantiate(straight, pos, Quaternion.Euler(0, 90, 0));
+        }
+        else if (matchedPattern == cornerLeftBottom || matchedPattern == cornerLeftTop || matchedPattern == cornerRightBottom || matchedPattern == cornerRightTop)
+        {
+            Quaternion rot = GetCornerRotation(matchedPattern);
+            Instantiate(GetCornerPiece(matchedPattern), pos, rot);
+        }
+        else if (matchedPattern == crossroadsPattern)
+        {
+            Instantiate(crossroads, pos, Quaternion.identity);
+        }
+        else if (matchedPattern == deadendBottom || matchedPattern == deadendLeft || matchedPattern == deadendRight || matchedPattern == deadendTop)
+        {
+            Instantiate(deadend, pos, GetDeadendRotation(matchedPattern));
+        }
+        else if (matchedPattern == junctionBottom || matchedPattern == junctionLeft || matchedPattern == junctionRight || matchedPattern == junctionTop)
+        {
+            Instantiate(junction, pos, GetJunctionRotation(matchedPattern));
+        }
     }
 
-    bool ShouldPlaceHorizontalPiece(int x, int z)
+    private GameObject GetCornerPiece(int[] pattern)
     {
-        return Search2D(x, z, new int[] { 5, 1, 5, 0, 0, 0, 5, 1, 5 });
+        if (pattern == cornerLeftBottom || pattern == cornerLeftTop)
+            return cornerStraight;
+        else
+            return cornerCurved;
     }
 
-    bool Search2D(int cellPos, int rowPos, int[] pattern)
+    private Quaternion GetCornerRotation(int[] pattern)
+    {
+        Quaternion val = Quaternion.identity;
+        if (pattern == cornerLeftTop)
+            val = Quaternion.Euler(0, -90, 0);
+        else if (pattern == cornerRightTop)
+            val = Quaternion.Euler(0, 90, 0);
+        else if (pattern == cornerLeftBottom)
+            val = Quaternion.Euler(0, 180, 0);
+        else if (pattern == cornerRightBottom)
+            val = Quaternion.Euler(0, 180, 0);
+        return val;
+    }
+
+    private Quaternion GetDeadendRotation(int[] pattern)
+    {
+        Quaternion val = Quaternion.identity;
+        if (pattern == deadendLeft)
+            val = Quaternion.Euler(0, 90, 0);
+        else if (pattern == deadendRight)
+            val = Quaternion.Euler(0, -90, 0);
+        else if (pattern == deadendTop)
+            val = Quaternion.Euler(0, 180, 0);
+        return val;
+    }
+
+    private Quaternion GetJunctionRotation(int[] pattern)
+    {
+        Quaternion val = Quaternion.identity;
+        if (pattern == junctionBottom)
+            val = Quaternion.Euler(0, 90, 0);
+        else if (pattern == junctionLeft)
+            val = Quaternion.Euler(0, 180, 0);
+        else if (pattern == junctionTop)
+            val = Quaternion.Euler(0, -90, 0);
+        return val;
+    }
+
+    private int[] HasMatchedPattern(int x, int z, int[][] possibleMatches)
+    {
+        int[] matchedPattern = null;
+        for (int i = 0; i < possibleMatches.Length; i++)
+        {
+            matchedPattern = Search2D(x, z, possibleMatches[i]);
+            if (matchedPattern != null)
+                break;
+        }
+        return matchedPattern;
+    }
+
+    int[] Search2D(int cellPos, int rowPos, int[] pattern)
     {
         int matches = 0;
         int pos = 0;
@@ -104,12 +210,12 @@ public class Maze : MonoBehaviour
         {
             for (int x = -1; x < 2; x++)
             {
-                if (pattern[pos] == map[cellPos+x, rowPos+z] || pattern[pos] == wildcard)
+                if (pattern[pos] == map[cellPos + x, rowPos + z] || pattern[pos] == wildcard)
                     matches++;
                 pos++;
             }
         }
-        return (matches == 9);
+        return matches == 9 ? pattern : null;
     }
 
     public int CountSquareNeighbours(int x, int z)
@@ -136,6 +242,6 @@ public class Maze : MonoBehaviour
 
     public int CountAllNeighbours(int x, int z)
     {
-        return CountSquareNeighbours(x,z) + CountDiagonalNeighbours(x,z);
+        return CountSquareNeighbours(x, z) + CountDiagonalNeighbours(x, z);
     }
 }
